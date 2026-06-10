f5abb5e3619dc39582afd4e34a1b8586bbe0e7cd
Rado Nafi'an Sajid <dora@gt.student.pens.ac.id>
2026-06-10 08:33:12 +0700

    duar

---
+++ Assets/Script/MenuManager.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuUI : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(
+            "GameMode"
+        );
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void Level()
+    {
+        SceneManager.LoadScene(
+            "Level"
+        );
+    }
+    public void BackMain()
+    {
+        SceneManager.LoadScene(
+            "Main Menu"
+        );
+    }
+
+    public void Level1()
+    {
+        SceneManager.LoadScene(
+            "Level1"
+        );
+    }
+
+    public void BackMode()
+    {
+        SceneManager.LoadScene(
+            "GameMode"
+        );
+    }
+}
